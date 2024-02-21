dd188b97fb50685763e688c8cbe8391794d62139
azamatif <azamatif@code.edu.az>
2024-02-21 13:26:03 +0100

    #adding task

---
+++ method/Program.cs
@@ -0,0 +1,72 @@
+﻿Console.WriteLine("Hello, World!");
+
+#region
+
+//static string Print (string name,string surname,string address)
+//{
+//    return name + " "+surname +" "+ address;
+//}string result = Print("Arzu", "Farajova", "Strzelcow");
+//Console.WriteLine(result);
+#endregion
+#region Task2
+//int[] nums = { 13, 55, 27, 19, 11, 4, 16 };
+//Console.WriteLine(SumArrElements(nums));
+
+//static int SumArrElements (int[] arr)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        sum += arr[i];
+//    }
+//    return sum;
+//}
+
+
+#endregion
+#region Task1
+//static int TwoValue(int a , int b)
+//{
+//    return a / b;
+//}
+//Console.WriteLine(TwoValue(36, 6));
+
+#endregion
+#region Task3
+//n ve m arasinda nece tek eded oldugu
+//int n = 20;
+//int m = 12;
+//static int CountNums (int n,int m)
+//{
+//    int count = 0;
+
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i%2==1)
+//        {
+//            count++;
+//        }
+//    }return count;
+//}
+//Console.WriteLine(CountNums);
+
+#endregion
+#region Task4
+//int num = 19;
+//static int ChekhType (int num)
+//{
+
+//    for (int i = 0; i < num; i++)
+//    {
+//        if (num % i == 0)
+//        {
+//            return 0;// eded murekkebdir
+//        }
+//    }return 1; //eded sadedir
+
+//}
+//Console.WriteLine(ChekhType);
+
+
+#endregion
+
